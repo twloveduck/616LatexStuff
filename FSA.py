2d7ee12697c118bc5f47b9b410905121d9d59232
Tuck Williamson <tuckwilliamson@gmail.com>
2024-02-27 17:14:00 -0500

    Class implementation. Not quite working.

--- FSA.py
+++ FSA.py
@@ -61,6 +61,7 @@ import dataclasses
 import string
 import typing
 import weakref
+from builtins import int
 
 import pyperclip
 
@@ -106,7 +107,8 @@ def state(prefix: str, idx: int | str) -> str:
     :param idx: The index of this state in the FSA machine.
     :return: A string representing the name of a state in the FSA machine.
     """
-    return f"q_{'{'}{prefix}.{idx}{'}'}"
+    return f"q^{Brace(prefix)}_{Brace(str(idx))}"
+    # return f"q_{'{'}{prefix}.{idx}{'}'}"
 
 
 def state_2(name: str, idx: int | str) -> str:
@@ -116,9 +118,11 @@ def state_2(name: str, idx: int | str) -> str:
 machTemplate: string.Template = (
         string.Template(
                 "M_{$name} = \\t{Q_{$name}, \\Sigma, \\delta_{$name}, $q0, F_{$name}} = "
-                "\\t{\\s{$q0, $qf}, \\Sigma, \\s{$transitions}, $q0, \\s{$qf}}\\\\\n"))
+                "\\t{\\s{$states}, \\Sigma, \\s{$transitions}, $q0, \\s{$qf}}\\\\\n"))
 
 onType = str | typing.Iterable[str]
+
+transitions_type = (list[tuple[int | str, onType, int | str]] | onType)
 
 
 @dataclasses.dataclass
@@ -134,50 +138,112 @@ class Delta:
     def __str__(self) -> str:
         return f"{self.fromState}\\xmapsto{Brace(self.on)} {self.toState}"
 
-transitions_type = (list[tuple[int | str, onType, int | str]] | onType)
-def Machine(name: str, transitions: transitions_type) -> str:
-    states: set[str] = set[str]()
-    deltasTable: list[Delta] = []
-    prefix = idx_2(name)
-    match transitions:
-        # Determine which type of transition it is and handle it.
-        case [*deltas]:
-            for delta in deltas:
-                match delta:
-                    case (fromIdx, onType as on, toIdx):
-                        def match_to_state(possIdx: int | str) -> str:
-                            match possIdx:
-                                case '0' | 'f' | int(_):
-                                    # Add my prefix and create a state.
-                                    return state(prefix, possIdx)
-                                case str():
-                                    # This is already a fully formatted state.
-                                    return possIdx
-                                case _:
-                                    raise ValueError(f"Unrecognized transition element {possIdx} in transition {delta}")
+    @classmethod
+    def from_tuples(cls, prefix: str, transitions: transitions_type) -> list['Delta']:
+        deltasTable: list['Delta'] = []
+        match transitions:
+            # Determine which type of transition it is and handle it.
+            case [*deltas]:
+                # List of state transitions.
+                for delta in deltas:
+                    match delta:
+                        case (fromIdx, onType as on, toIdx):
+                            def match_to_state(possIdx: int | str) -> str:
+                                match possIdx:
+                                    case '0' | 'f' | int(_):
+                                        # Add my prefix and create a state.
+                                        return state(prefix, possIdx)
+                                    case str():
+                                        # This is already a fully formatted state.
+                                        return possIdx
+                                    case _:
+                                        raise ValueError(
+                                                f"Unrecognized transition element {possIdx} in transition {delta}")
 
-                        states.add(fromName := match_to_state(fromIdx))
-                        states.add(toName := match_to_state(toIdx))
-                        deltasTable.append(Delta(fromName, on, toName))
-                    case _:
-                        raise ValueError(f"No match found for {delta}.")
-                # case ('0' as fromIdx, onType(on), str(toName)):
-                #     states.add(fromName := state(prefix, fromIdx))
-                #     states.add(toName)
-                #     deltas.append(Delta(fromName, on, toName))
-                # case (str(fromName), onType(on), str(toName)):
-                #     states.add(fromName)
-                #     states.add(toName)
-                #     deltas.append(Delta(fromName, on, toName))
-        case str(on):
-            states.add(fromName := state(prefix, 0))
-            states.add(toName := state(prefix, 'f'))
-            deltasTable.append(Delta(fromName, on, toName))
-        case _:
-            raise ValueError(f"No match found for {transitions}.")
-    xlist = ', '.join([str(delta) for delta in deltasTable])
+                            # states.add(fromName := match_to_state(fromIdx))
+                            # states.add(toName := match_to_state(toIdx))
+                            fromName = match_to_state(fromIdx)
+                            toName = match_to_state(toIdx)
+                            deltasTable.append(Delta(fromName, on, toName))
+                        case _:
+                            raise ValueError(f"No match found for {delta}.")
+            case str(on):
+                # states.add(fromName := state(prefix, 0))
+                # states.add(toName := state(prefix, 'f'))
+                fromName = state(prefix, 0)
+                toName = state(prefix, 'f')
+                deltasTable.append(Delta(fromName, on, toName))
+            case _:
+                raise ValueError(f"No match found for {transitions}.")
+        return deltasTable
 
-    return machTemplate.substitute(name=prefix, transitions=xlist, q0=state(prefix, 0), qf=state(prefix, 'f'))
+
+@dataclasses.dataclass
+class Machine:
+    name: str
+    transitions: transitions_type
+    parentMachine: 'Machine' = dataclasses.field(default=None, init=False)
+    nestLevel: int = dataclasses.field(default=None, init=False)
+    prefix: str = dataclasses.field(default=None, init=False)
+    deltasTable: list[Delta] = dataclasses.field(default_factory=list, init=False)
+    states: set[str] = dataclasses.field(default_factory=set, init=False, repr=False)
+    initComplete: bool = False
+
+    # @classmethod
+    # def __reset__(cls):
+    @classmethod
+    def __get_root_source__(cls):
+        rootSrc = list('abcde')
+        yield rootSrc.pop(0)
+
+    def get_next_index(self):
+        if self.nestLevel == 0:
+            return self.__class__.__get_root_source__()
+        else:
+            source = list('abcde') if self.nestLevel % 2 == 0 else range(10)
+            yield source.pop(0)#str(next(source))
+
+    def __post_init__(self):
+        if self.parentMachine is None:
+            nestLevel = 0
+        else:
+            nestLevel = 0
+            while parent := self.parentMachine is not None:
+                nestLevel += 1
+                parent = parent.parentMachine
+
+    def do_init(self):
+        if self.initComplete:
+            return
+        # if self.name is not None:
+        #     self.prefix = self.name
+        # else:
+        #     self.name = next(self.parentMachine.get_next_index() if self.parentMachine else self.get_next_index())
+        self.prefix = idx_2(self.name)
+
+        deltasTable = Delta.from_tuples(self.prefix, self.transitions)
+
+        for delta in deltasTable:
+            self.states.add(delta.fromState)
+            self.states.add(delta.toState)
+
+    def __str__(self):
+        self.do_init()
+
+        xlist = ', '.join([str(delta) for delta in self.deltasTable])
+        return machTemplate.substitute(name=self.prefix, transitions=xlist, q0=state(self.prefix, 0),
+                                       qf=state(self.prefix, 'f'))
+
+    # def __add__(self, other) -> 'Machine':
+    #     # This is the union. New q0 epsilon transitions to lhs q_0 and rhs q_0.
+    #     # New q_f is epsilon transitioned to from lhs&rhs q_f
+    #
+    #     retVal = Machine(self.transitions + other.transitions)
+    #     self.parentMachine = retVal
+    #     other.parentMachine = retVal
+    #     return retVal
+
+# def m_cat(name: str, lhs: transitions_type, rhs: transitions_type) -> transitions_type:
 
 
 # Tests
@@ -189,13 +255,14 @@ print(Delta(state('1', 0), ['a', epsilonElement], state('1', 1)))
 
 outputStr = ''
 
-def print_mach(*mach: Machine.__annotations__, reset:bool = False):
+
+def print_mach(mach: Machine, reset: bool = False):
     global outputStr
     if reset:
         outputStr = ''
-    curMach = Machine(*mach)
-    print(f"Machine {mach!r}:\n{curMach}\n\n")
-    outputStr += curMach
+    # mach
+    print(f"Machine {mach!r}:\n{mach!s}\n\n")
+    outputStr += str(mach)
 
 
 # Test a bunch of error conditions.
@@ -212,9 +279,9 @@ for mach in testBadMachines:
     except Exception as e:
         print(f"Good catch for machine{mach!r}: {e}")
 
-print_mach('a', 'a')
-print_mach('a2a', 'a')
-print_mach('a2b', [(0, 'b', 1), (1, 'c', 'f')])
+print_mach(Machine('a', 'a'))
+print_mach(Machine('a2a', 'a'))
+print_mach(Machine('a2b', [(0, 'b', 1), (1, 'c', 'f')]))
 print_mach('a2', [(0, epsilonElement, state_2('a2a', 0)), (state_2('a2b', 'f'), epsilonElement, 'f')])
 
 #  (a + bc)(a + b)*
@@ -229,8 +296,7 @@ print_mach('a', [
         (state_2('a2', 'f'), epsilonElement, 'f')
 ])
 
-
 # def mach_cat(name: str, machAdeltas: transitions_type, machBdeltas: transitions_type):
 #     print(machAdeltas)
 
-pyperclip.copy(outputStr)
+pyperclip.copy(outputStr.strip())
